dbca7bdd68d94819909f10233d0c737f6308c547
adrianalynn <35774920+adrianalynn@users.noreply.github.com>
2018-03-07 12:11:56 -0600

    final product upload
    Added a photo to appear/disappear when the photo link is clicked

--- dynamic page.js
+++ dynamic page.js
@@ -1,17 +1,24 @@
 // JavaScript Document
+var adriana = document.getElementById("adriana")
+var adrianaPic = document.getElementById("adriana-pic")
+adriana.addEventListener("click", function(){
+	if (adrianaPic.className === "hide"){
+		adrianaPic.className = "";
+	} else {
+		adrianaPic.className = "hide";
+	}
+});
+
 document.getElementById("homeMe").addEventListener("click",funHome );
 document.getElementById("contactMe").addEventListener("click",funContact );
 document.getElementById("aboutMe").addEventListener("click",funAbout );
-document.getElementById("photoMe").addEventListener("click",funPhoto );
+
 function funHome (){
-	document.getElementById("homePageText").innerHTML = "Welcome to my home page!";
+	document.getElementById("homePageText").innerHTML = "Welcome to my page!";
 }
 function funContact (){
 	document.getElementById("homePageText").innerHTML = "To contact me....";
 }
 function funAbout (){
 	document.getElementById("homePageText").innerHTML = "About me....";
-}
-function funPhoto (){
-	document.getElementById("homePageText").innerHTML = "A picture of me....";
 }
